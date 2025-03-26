77b697ae11c7649139a02338036d3cf692a3abb3
Tran Gia Tien Dat <117809627+trangiatiendat@users.noreply.github.com>
2025-03-26 23:10:27 +0700

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const productController = require("../controllers/productController");
+const productController = require("./controllers/productController");
 
 // Định tuyến API
 router.get("/", productController.getAllProducts);
